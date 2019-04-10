ddfc24f854d0ffa6109ce6f17a60b5944b08acdb
joshuabyker <47902849+joshuabyker@users.noreply.github.com>
2019-04-09 18:53:00 -0700

    2nd

---
+++ assets/trivia.js
@@ -0,0 +1,97 @@
+var FrodosSword;
+var Anakin;
+var wand;
+var Robin;
+var Identity
+var Naruto;
+var Matrix;
+var Dog;
+var Ford;
+var actor;
+var song;
+var UserGuess;
+var Answer;
+var RightAnswer;
+var WrongAnswer
+
+$("#num1");{
+    
+if(UserGuess==true){
+RightAnswer++;
+}else(WrongAnswer++);
+}
+
+$("#num2");{
+    
+    if(UserGuess==true){
+        RightAnswer++;
+    }else(WrongAnswer++);
+}
+
+$("#num3");{
+
+    if(UserGuess==true){
+        RightAnswer++;
+
+    }else(WrongAnswer++);
+}
+
+$("#num4");{
+    if(UserGuess==true);{
+        RightAnswer++;
+    }else(WrongAnswer++)++
+}
+
+$("#num5");{
+    if(UserGuess==true){
+        RightAnswer++;
+
+    }else(WrongAnswer++);
+}
+
+
+$("#num6");{
+    if(UserGuess==true){
+        RightAnswer++;
+
+    }else(WrongAnswer++);
+}
+
+$("#num7");{
+    if(UserGuess==true){
+        RightAnswer++;
+
+    }else(WrongAnswer++);
+}
+
+$("#num8");{
+    if(UserGuess==true){
+        RightAnswer++;
+
+    }else(WrongAnswer++);
+}
+
+$("#num9");{
+    if(UserGuess==true){
+        RightAnswer++;
+
+    }else(WrongAnswer++);
+}
+
+$("#num9");{
+    if(UserGuess==true){
+        RightAnswer++;
+
+    }else(WrongAnswer++);
+}
+
+$("#num10");{
+    if(UserGuess==true){
+        RightAnswer++;
+
+    }else(WrongAnswer++);
+}
+
+
+
+
