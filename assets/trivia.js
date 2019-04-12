41f1e22849a936fdb9c435d2a95819a8ff62e561
joshuabyker <47902849+joshuabyker@users.noreply.github.com>
2019-04-12 11:20:44 -0700

    almost

--- assets/trivia.js
+++ assets/trivia.js
@@ -1,43 +1,41 @@
+var NumOfQuestions = 10;
+var NumofChoices = 3;
+var WrongAnswers = 0;
+var CorrectAnswers = 0;
+var currentSelection="";
+var Answer = new Array(10);
+Answer[0] = "Sting";
+Answer[1] = "Blue";
+Answer[2] = "Holly";
+Answer[3] = "DickGrayson";
+Answer[4] = "Thor";
+Answer[5] = "ShadowClonejutsu";
+Answer[6] = "KeanuReeves";
+Answer[7] = "Pluto";
+Answer[8] = "IndianaJones";
+Answer[9] = "Europe";
 
 
 
-  var NumOfQuestions=10;
-  var NumofChoices=3;
-  var WrongAnswers=0;
-  var CorrectAnswers=0;
-  var Answers= new Array(10);
-  Answers[0]="Sting"    ;
-  Answers[1]="Blue";
-  Answers[2]="Holly";
-  Answers[3]="DickGrayson";
-  Answers[4]="Thor";
-  Answers[5]="ShadowClonejutsu";
-  Answers[6]="KeanuReeves";
-  Answers[7]="Pluto";
-  Answers[8]="IndianaJones";
-  Answers[9]="Europe";
-  function checkAnswer(form){
-    let i=0;
-    var currentSelection;
-      for(i=0;i<Answers.length;i++){
-          if(currentSelection == Answers){
-              CorrectAnswers++;
-          }else(CorrectAnswers+0)
-      }
-      if(currentSelection===!Answers){
-          WrongAnswers++;
-      }
 
-  }
+$(".btn").on("click", function (event) {
+    console.log("I've Been clicked");
+    
+    for (i = 0; i < Answer.length; i++) {
+        if (currentSelection == Answer) {
+            CorrectAnswers++;
+        } else{}
+    }
 
-score = Math.floor(CorrectAnswers/NumOfQuestions*100);
-form.percentage.value == score+ "%";
+    function checkAnswer() {
+        score = Math.floor(CorrectAnswers / NumOfQuestions * 100);
+    alert ("You got a" + score);
 
-$("submit").on("click",function(){
-    console.log("AUGH, I've Been clicked")
-});
+    }
+}
 
-
+    
+   
 
 
 
@@ -65,4 +63,4 @@ $("submit").on("click",function(){
 
 
 
-
+)
